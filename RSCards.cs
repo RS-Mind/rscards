69c032efd04026b17fb62b4e6f4490921be5bb14
RS_Mind <jared@awoodfamily.com>
2026-08-01 16:27:20 -0600

    1.7.5
    I forgot to rebuild assetbundles

--- RSCards.cs
+++ RSCards.cs
@@ -22,7 +22,7 @@ namespace RSCards
     {
         private const string ModId = "com.rsmind.rounds.RSCards";
         private const string ModName = "RSCards";
-        public const string Version = "1.7.4";
+        public const string Version = "1.7.5";
         public const string ModInitials = "RSC";
         private CardHolder cardHolder;
         public static RSCards instance { get; private set; }
